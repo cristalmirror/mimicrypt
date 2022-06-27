a0ab427e29dc8094977c65ab589dbd491d1f838b
crisalmirror <crisal@debian>
2022-06-27 13:05:06 -0300

    we create a header thar make parts of de RNG for more simple reading code

---
+++ list_num_used.h
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct list_num{
+
+      int num;
+      struct list_num *sig;
+      
+};
+
+void create_list(struct list_num *data1);
+
+void create_list(struct list_num *data1){
+
+      *date=NULL;
+      
+}
--- mimicrypt.c
+++ mimicrypt.c
@@ -3,15 +3,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include "list_num_used.h"
 
 //create a aleatory numbers for each letter in the text
-int rand_num(time x_n,FILE tam);
+int rand_num(time x_n);
 
 //main program
 int main(int argc,char *argv[]){
       //defines of constants an variable
       int res,cont;
-      time *date=time(NULL),aux_date;
+      time date=time(NULL),aux_date;
       FILE *d_text= NULL;
 
       //save the public key based in the time
@@ -24,7 +25,7 @@ int main(int argc,char *argv[]){
       while(1==feof(tam)){
 
 	    //create a aleatory numbers for each letter in the text
-	    res=rand_num(&date,d_text);
+	    res=rand_num(date,d_text);
 
 	    
       }
@@ -33,18 +34,20 @@ int main(int argc,char *argv[]){
 }
 
 //create a aleatory numbers for each letter in the text
-int rand_num(time *x_n){
+int rand_num(time x_n){
       long int a=31415926535,c=27182818284,m=1414213562373;
       int result;
       bool opc=true;
 
       //generate numbers that overcome the 255 
       while(opc){
-	    result=(a*(*x_n)+c)%m;//generator of aletory numbers
-	    *x_n=result;
+	    result=(a*(x_n)+c)%m;//generator of aletory numbers
+	    x_n=result;
+
+	    result=result/10000000;
 
 	    //test that are in range ASCII
-	    if(result<=255){
+	    if((result<=255)&&(result>-1)){
 
 		  Opc=false;
 		  return result;
