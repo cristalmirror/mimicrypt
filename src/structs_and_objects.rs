3f99fd67c15390415a7e7c909d4945fbfd88d488
crisal <crisal@debian-BULLSEYE-live-builder-AMD64>
2022-11-17 22:33:56 -0300

    rust rescritura

---
+++ src/structs_and_objects.rs
@@ -0,0 +1,23 @@
+//struct of the object that encrypt archive
+
+struct Ecrypt{
+    key: u64,
+
+}
+
+struct Decrypt{
+
+    respetion_key: u64,
+    
+}
+
+//methods
+impl Encript{
+
+    fn gen_aleatory_key()->{
+
+	
+	
+    }
+
+}
